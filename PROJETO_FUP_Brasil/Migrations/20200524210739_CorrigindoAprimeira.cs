1175400626dd6c82e746a7d41818eca24bcd4f0f
Dionney <dionneyscrok15@gmail.com>
2020-05-24 21:20:24 -0300

    Projeto 24 05 2020

---
+++ PROJETO_FUP_Brasil/Migrations/20200524210739_CorrigindoAprimeira.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PROJETO_FUP_Brasil.Migrations
+{
+    public partial class CorrigindoAprimeira : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DataTerminoCurso",
+                table: "Aluno",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DataTerminoCurso",
+                table: "Aluno",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
